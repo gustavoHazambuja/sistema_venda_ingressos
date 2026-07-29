a80a2b917c51eab61c842ad2a712de1f368b7361
Gustavo Henrique <guazambujinha@gmail.com>
2026-07-29 19:26:20 -0300

    fist commi

---
+++ sistema_venda_ingressos/src/main/java/com/example/sistema_venda_ingressos/SistemaVendaIngressosApplication.java
@@ -0,0 +1,13 @@
+package com.example.sistema_venda_ingressos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaVendaIngressosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaVendaIngressosApplication.class, args);
+	}
+
+}
---
+++ sistema_venda_ingressos/src/test/java/com/example/sistema_venda_ingressos/SistemaVendaIngressosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sistema_venda_ingressos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaVendaIngressosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
